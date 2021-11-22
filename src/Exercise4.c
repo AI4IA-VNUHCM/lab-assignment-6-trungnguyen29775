709fd59b0685f2308d0df30bc27f3ecd64727517
NguyenTranMinhTrungITITIU20332 <91869278+NguyenTranMinhTrungITITIU20332@users.noreply.github.com>
2021-11-22 18:38:10 +0700

    42

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -29,18 +29,31 @@ void Ex4(char *str){
 		len -= 1;
 		j = len-1;
 	}
+	char str2[100];
+	for (int e = 0; i < len; e++)
+		str2[e] = str[e];
+	int check = 0;
 	for (int i = 0; i < len; i++)
 	{
 		if (str[i] == ' ' && str[i + 1] == ' ')
 		{
-			for (int e = i; e < len - 1; e++)
-				str[e] = str[e] + 1;
-			len -= 1;
+			if (check == 1)
+			{
+				for (int e = i-1; e < len - 1; e++)
+					str2[e] = str2[e] + 1;
+				len -= 1;
+			}
+			else
+			{
+				for (int e = i ; e < len - 1; e++)
+					str2[e] = str2[e] + 1;
+				len -= 1;
+			}
+			check = 1;
 		}
 	}
 	for (int e = 0; e < len; e++)
 		printf("%c", str[i]);
-	
 }
 
 int main(int argc, char *argv[]) {

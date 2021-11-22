bdb64719cca6418ced024db438ea63d3f2642b7c
NguyenTranMinhTrungITITIU20332 <91869278+NguyenTranMinhTrungITITIU20332@users.noreply.github.com>
2021-11-22 16:16:30 +0700

    13

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -11,9 +11,39 @@ Ex:
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-
 void Ex2(int n, char *str[]){
 	//Your codes here
+	char **StrArr[100];
+	int count=0;
+	while(str[count]!=" ")
+		count+=1;
+	for(int i=0;i<count;i++)
+		StrArr[0][i]=str[i];
+	count=0;
+	for(int i=1;i<n;i++)
+	{
+		for(int j=count;j<strlen(str);j++)
+			if(str[j]==" "){
+				int Len=0;
+				while(str[Len]!=" " || str[Len]!= '\0')
+					Len+=1;
+				for(int e=0;e<Len;e++)
+					StrArr[i][e]=str[e+j+1];
+			}
+	}
+	for(int i=0;i<n-1;i++)
+		for(int j=i+1;j<n;j++)
+		{
+			if(strcmp(StrArr[i],StrArr[j])>0)
+			{
+				char *temp[100];
+				strcpy(temp,StrArr[i]);
+				strcpy(StrArr[i],StrArr[j]);
+				strcpy(StrArr[j],temp);
+			}
+		}
+	for(int i=0;i<n;i++)
+		printf("%s ",StrArr[i]);
 	
 }
 

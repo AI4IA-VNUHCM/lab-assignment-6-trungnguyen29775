460d2f0128ff8e922229ad787d41682f9145c3ac
NguyenTranMinhTrungITITIU20332 <91869278+NguyenTranMinhTrungITITIU20332@users.noreply.github.com>
2021-11-22 19:10:05 +0700

    23

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -24,14 +24,13 @@ int Strcmp(char str1[100], char str2[100])
 	return 0;
 }
 void Ex2(int n, char* str[]) {
-	//Your codes here
 	for(int i=0;i<n-1;i++)
 		for (int j = i + 1; j < n; j++)
 		{
 			if (Strcmp(str[i], str[j]) == 1)
 			{
 				char temp[100];
-				for (int e = 0; i < strlen(str[i]); i++)
+				for (int e = 0; e < strlen(str[i]); e++)
 					temp[e] = str[i][e];
 				for (int e = 0; e < strlen(str[j]); e++)
 				{
@@ -40,8 +39,8 @@ void Ex2(int n, char* str[]) {
 				}
 				for (int e = 0; e < strlen(str[j]); e++)
 				{
-					str[j][e] = str[i][e];
-					str[j][strlen(str[i])] = '\0';
+					str[j][e] = temp[e];
+					str[j][strlen(temp)] = '\0';
 				}
 			}
 		}
